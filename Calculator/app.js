4dbb487f4428b629340547c81d7a244f4d24f0c7
takrim1999 <takrimulalom@gmail.com>
2024-12-04 15:37:07 +0600

    Fixed Issue 1

--- Calculator/app.js
+++ Calculator/app.js
@@ -3,24 +3,35 @@ let buttons = document.querySelectorAll('button')
 
 let string = ''
 
-buttons.forEach(e =>{
-    e.addEventListener('click', (b)=>{
+buttons.forEach(e => {
+    ResultShowingFlag = 0
+    e.addEventListener('click', (b) => {
         // console.log(string)
-        if(b.target.innerText == '='){
-            string = String(Math.round(eval(string)*10000)/10000)
+
+        if (b.target.innerText == '=') {
+            string = String(Math.round(eval(string) * 10000) / 10000)
+            ResultShowingFlag = 1
             inputBox.value = string;
         }
-        else if(b.target.innerText == 'AC'){
+        else if (b.target.innerText == 'AC') {
             string = ''
             inputBox.value = string
         }
-        else if(b.target.innerText == 'DEL'){
-            string = string.substring(0,string.length-1)
+        else if (b.target.innerText == 'DEL') {
+            string = string.substring(0, string.length - 1)
             inputBox.value = string
         }
-        else{
-            string += b.target.innerText
-            inputBox.value = string
+        else {
+            if (ResultShowingFlag == 0) {
+                string += b.target.innerText
+                inputBox.value = string
+            }
+            else {
+                string = ''
+                string += b.target.innerText
+                inputBox.value = string
+                ResultShowingFlag = 0
+            }
         }
     })
 })
